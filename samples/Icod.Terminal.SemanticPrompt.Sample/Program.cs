63dbc8a3509fdcc2b418557e0f0f1bcd0b3a59c7
Timothy Bruce <uniblab@hotmail.com>
2026-09-05 09:40:58 -0400

    Add 0.12 semantic-prompt sample

---
+++ samples/Icod.Terminal.SemanticPrompt.Sample/Program.cs
@@ -0,0 +1,36 @@
+using Icod.Terminal;
+
+await using TerminalSession session = await TerminalSession.OpenAsync(
+	new TerminalSessionOptions {
+		InputMode = TerminalInputMode.CBreak,
+		EchoInput = false
+	}
+);
+
+await session.BeginPromptAsync();
+await session.WriteTextAsync(
+	"demo> "
+);
+
+await session.BeginCommandInputAsync();
+await session.WriteTextAsync(
+	"echo hello\r\n"
+);
+
+await session.BeginCommandOutputAsync();
+await session.WriteTextAsync(
+	"hello\r\n"
+);
+
+await session.FinishCommandAsync( 0 );
+
+await session.BeginPromptAsync();
+await session.WriteTextAsync(
+	"demo> cancelled command\r\n"
+);
+await session.BeginCommandInputAsync();
+await session.AbortCommandAsync();
+
+await session.WriteTextAsync(
+	"OSC 133 semantic-prompt sample complete.\r\n"
+);
