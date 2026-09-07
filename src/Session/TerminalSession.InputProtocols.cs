f493a3c272cea8d8c2cd522cf7bbf84532507ae1
Timothy Bruce <uniblab@hotmail.com>
2026-09-07 08:56:26 -0400

    T182: include teardown in rich-input acquisition barrier

--- src/Session/TerminalSession.InputProtocols.cs
+++ src/Session/TerminalSession.InputProtocols.cs
@@ -26,7 +26,7 @@ public sealed partial class TerminalSession {
 		using IDisposable composition = await this.AcquireStateCompositionAsync(
 			cancellationToken
 		).ConfigureAwait( false );
-		this.ThrowIfStateAcquisitionUnavailableForLifecycle();
+		this.ThrowIfStateAcquisitionUnavailable();
 
 		if ( options.KeyboardReportingMode.HasValue ) {
 			bool kittySupported = await this.ProbeKittyKeyboardSupportAsync(
