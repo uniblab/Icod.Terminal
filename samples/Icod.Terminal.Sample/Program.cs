c51b33b8fcaf03c13061ef714fc934012e2444ce
Timothy Bruce <uniblab@hotmail.com>
2026-09-08 02:42:58 -0400

    1.0.0: clarify basic sample license description

--- samples/Icod.Terminal.Sample/Program.cs
+++ samples/Icod.Terminal.Sample/Program.cs
@@ -1,6 +1,6 @@
 /*
 	Icod.Terminal.Sample
-	Sample application demonstrating Icod.Terminal Sample features.
+	Basic sample application demonstrating Icod.Terminal session usage.
 	Copyright (C) 2026  Timothy J. Bruce <uniblab@hotmail.com>
 */
 
@@ -28,27 +28,12 @@ await using TerminalSession session = await TerminalSession.OpenAsync(
 	}
 );
 
-TerminalControlResult<TerminalSize> sizeResult = session.GetSize();
-string sizeText;
-if ( sizeResult.IsAvailable ) {
-	TerminalSize size = sizeResult.GetRequiredValue();
-	sizeText = string.Concat(
-		size.Columns.ToString( System.Globalization.CultureInfo.InvariantCulture ),
-		"x",
-		size.Rows.ToString( System.Globalization.CultureInfo.InvariantCulture )
-	);
-} else {
-	sizeText = sizeResult.Status.ToString();
-}
+await session.WriteTextAsync( "Icod.Terminal sample ready.\r\n" );
 
-await session.WriteTextAsync(
-	string.Concat(
-		"Icod.Terminal session opened via ",
-		session.Identity.Source.ToString(),
-		"; size=",
-		sizeText,
-		".\r\n"
-	)
+TerminalEvent terminalEvent = await session.ReadEventAsync(
+	TimeSpan.FromSeconds( 1 )
 );
 
-return 0;
+if ( terminalEvent.Kind == TerminalEventKind.Input ) {
+	await session.WriteTextAsync( $"Input: {terminalEvent.Input.Kind}\r\n" );
+}
